9863e41839f392827ec5a515989ac64af26e9d75
Robert Young <youngrobert4321@gmail.com>
2020-03-23 12:47:07 -0500

    Created and finished the program

---
+++ Participations/Participations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Participations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a type of animal. (cow, snake, dog, cat, etc) >>");
+            string AnimalType = Console.ReadLine().ToLower();
+
+            string noise = Speak(AnimalType);
+
+            Console.WriteLine(noise);
+
+            Console.ReadKey();
+        }
+        static string Speak(string AnimalType)
+        {
+            if (AnimalType == "cow")
+            {
+                return "Moo";
+            }
+            else if (AnimalType == "cat")
+            {
+                return "Meow";
+            }
+            else if (AnimalType == "dog")
+            {
+                return "Woof";
+            }
+            else if (AnimalType == "snake")
+            {
+                return "Hiss";
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that animal's information is not avaliable.");
+            }
+            return "Sorry, that animal's information is not avaliable.";
+        }
+    }
+}
