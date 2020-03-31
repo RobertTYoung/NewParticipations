9f7cc78538c0ec3caa02d7cb6ed0192e3908113e
Robert Young <youngrobert4321@gmail.com>
2020-03-30 21:53:48 -0500

    Almost done, jsut can't get padding correct

--- Participations/(P) Classes/Program.cs
+++ Participations/(P) Classes/Program.cs
@@ -6,8 +6,39 @@ namespace _P__Classes
     {//Robert Young
         static void Main(string[] args)
         {
+            int padNum;
+            char pad;
+            ConsoleColor backColor, foreColor;
+            string text, Newtext;
+
+            TextBox TB1 = new TextBox();
+            padNum = 2;
+            pad = '-';
+            backColor = ConsoleColor.Red;
+            foreColor = ConsoleColor.White;
+            text = "Happy Birthday World!";
+
+            Console.BackgroundColor = backColor;
+            Console.ForegroundColor = foreColor;
+
+            Newtext = text.PadLeft(padNum);
+            Console.WriteLine(Newtext.PadLeft(padNum));
+
+            TextBox TB2 = new TextBox();
+            padNum = 8;
+            pad = '-';
+            backColor = ConsoleColor.Green;
+            foreColor = ConsoleColor.Blue;
+            text = "Happy Midnight World!";
+
+            Console.BackgroundColor = backColor;
+            Console.ForegroundColor = foreColor;
+
+            Newtext = text.PadRight(padNum, pad);
+            Console.WriteLine(Newtext);
 
             Console.ReadKey();
         }
+
     }
 }
