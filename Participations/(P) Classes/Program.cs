541b7028291d30bd1e2ce6e0eaeab225cb41796b
Robert Young <youngrobert4321@gmail.com>
2020-03-30 21:18:12 -0500

    Created Class TextBox

---
+++ Participations/(P) Classes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _P__Classes
+{
+    class Program
+    {//Robert Young
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participations/(P) Classes/TextBox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P__Classes
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor ForeColor { get; set; }
+        public String Text { get; set; }
+
+
+        public TextBox()
+        {
+            Padding = 0;
+            BackColor = 0;
+            ForeColor = 0;
+            Text = " ";
+        }
+        public void DisplayText()
+        {
+
+        }
+
+    }
+}
