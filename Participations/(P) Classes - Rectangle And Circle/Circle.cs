f7e02f75ded764799c2c3fc5445a844c1650258f
Robert Young <youngrobert4321@gmail.com>
2020-04-01 19:06:24 -0500

    Created Circles Class

--- Participations/(P) Classes - Rectangle And Circle/Circle.cs
+++ Participations/(P) Classes - Rectangle And Circle/Circle.cs
@@ -6,5 +6,24 @@ namespace _P__Classes___Rectangle_And_Circle
 {
     class Circle
     {
+        public double Radius{ get; set; }
+
+        public Circle()
+        {
+            Radius = 0;
+        }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return (Math.PI) * (Radius * Radius);
+        }
+        public double CalculatePerimeter()
+        {
+            return (2*(Math.PI)) * Radius;
+        }
     }
 }
