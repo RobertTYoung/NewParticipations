8a7db831c704ea36eeb36cd0858117a81d19504e
Robert Young <youngrobert4321@gmail.com>
2020-04-01 20:28:48 -0500

    fixed wording at several points in the program

--- Participations/(P) Classes - Rectangle And Circle/Program.cs
+++ Participations/(P) Classes - Rectangle And Circle/Program.cs
@@ -15,24 +15,24 @@ namespace _P__Classes___Rectangle_And_Circle
             R1.Length = 10;
             R1.Width = 11;
 
-            Console.WriteLine($"Since first rectangles Length is {R1.Length} and the Width is {R1.Width} the Area is {R1.CalculateAreaR()}.");
-            Console.WriteLine($"Since first rectangles Length is {R1.Length} and the Width is {R1.Width} the Perimeter is {R1.CalculatePerimeterR()}.");
+            Console.WriteLine($"Since the first rectangles Length is {R1.Length} and the Width is {R1.Width} the Area is {R1.CalculateAreaR()}.");
+            Console.WriteLine($"Since the first rectangles Length is {R1.Length} and the Width is {R1.Width} the Perimeter is {R1.CalculatePerimeterR()}.");
 
             Rectangle R2 = new Rectangle(W, L);
 
-            Console.WriteLine($"Since first rectangles Length is {R2.Length} and the Width is {R2.Width} the Area is {R2.CalculateAreaR()}.");
-            Console.WriteLine($"Since first rectangles Length is {R2.Length} and the Width is {R2.Width} the Perimeter is {R2.CalculatePerimeterR()}.");
+            Console.WriteLine($"Since the first rectangles Length is {R2.Length} and the Width is {R2.Width} the Area is {R2.CalculateAreaR()}.");
+            Console.WriteLine($"Since the first rectangles Length is {R2.Length} and the Width is {R2.Width} the Perimeter is {R2.CalculatePerimeterR()}.");
 
             Circle C1 = new Circle();
             C1.Radius = 14;
 
-            Console.WriteLine($"The radius of teh the circleis {C1.Radius} so the Area is {C1.CalculateAreaC()}.");
-            Console.WriteLine($"The radius of teh the circleis {C1.Radius} so the Perimeter is {C1.CalculatePerimeterC()}.");
+            Console.WriteLine($"The radius of the the circleis {C1.Radius} so the Area is {C1.CalculateAreaC()}.");
+            Console.WriteLine($"The radius of the the circleis {C1.Radius} so the Perimeter is {C1.CalculatePerimeterC()}.");
 
             Circle C2 = new Circle(R);
 
-            Console.WriteLine($"The radius of teh the circleis {C2.Radius} so the Area is {C2.CalculateAreaC()}.");
-            Console.WriteLine($"The radius of teh the circleis {C2.Radius} so the Perimeter is {C2.CalculatePerimeterC()}.");
+            Console.WriteLine($"The radius of the the circleis {C2.Radius} so the Area is {C2.CalculateAreaC()}.");
+            Console.WriteLine($"The radius of the the circleis {C2.Radius} so the Perimeter is {C2.CalculatePerimeterC()}.");
 
             Console.ReadKey();
         }
