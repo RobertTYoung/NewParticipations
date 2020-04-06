443a3cf2042265f03a1a64ac7f04c126fe31c497
Robert Young <youngrobert4321@gmail.com>
2020-04-06 17:22:20 -0500

    Created student class

---
+++ Participations/(P) -/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P___
+{//Robert Young
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<string> CourseNames { get; set; }
+        public List<string> CourseGrades { get; set; }
+
+        public Student()
+        {
+            FirstName = " ";
+            LastName = " ";
+            CourseNames.Add(" ");
+            CourseGrades.Add(" ");           
+        }
+        public Student(string first, string second)
+        {
+            FirstName = first;
+            LastName = second;
+        }
+        public double CalculateGPA(double TotalPointsEarned, double TotalCreditsAttempted)
+        {
+            return (TotalPointsEarned / TotalCreditsAttempted);
+        }
+    }
+}
