5830fd0936ca57dc651f5c7442dbe3c1f6412429
Robert Young <youngrobert4321@gmail.com>
2020-04-08 18:57:02 -0500

    formatted part done correctly

--- Participations/(P) Classes 2 - Toys/Program.cs
+++ Participations/(P) Classes 2 - Toys/Program.cs
@@ -6,13 +6,6 @@ namespace _P__Classes_2___Toys
     {
         static void Main(string[] args)
         {
-            //string Manufacturer, Name, Aisle;
-            //double Price;
-
-            /*Manufacturer = "Nike";
-            Name = "Robert";
-            Price = 22.99;*/
-
             Toy T1 = new Toy();
             T1.Manufacturer = "Nike";
             T1.Name = "Truck";            
@@ -25,15 +18,13 @@ namespace _P__Classes_2___Toys
             T2.Name = "Robot";
             T2.Price = 62.99;
 
-            Console.WriteLine($"{T2.Manufacturer} makes a toy called {T2.Name} on aisle {T2.GetAisle(T2.Manufacturer)} and sells it for {(T2.Price).ToString("C2")}.");
+            Console.WriteLine($"\n{T2.Manufacturer} makes a toy called {T2.Name} on aisle {T2.GetAisle(T2.Manufacturer)} and sells it for {(T2.Price).ToString("C2")}.");
 
-
-            ToyBox TB1 = new ToyBox();
+            /*ToyBox TB1 = new ToyBox();
             TB1.Toys = new List<string>();
             TB1.Toys.Add(T1.Name);
             TB1.Toys.Add(T2.Name);
-            //TB1.Toys.Add("Robot");
-            Console.WriteLine( $"The list of Toy(s) include {TB1.Toys}.");
+            Console.WriteLine( $"The list of Toy(s) include {TB1.Toys}.");*/
 
             Console.ReadKey();
             
--- Participations/(P) Classes 2 - Toys/ToyBox.cs
+++ Participations/(P) Classes 2 - Toys/ToyBox.cs
@@ -9,16 +9,9 @@ namespace _P__Classes_2___Toys
         public List<string> Toys { get; set; }
         public string Name { get; }
 
-        public void ToyBox(string Name)
+        public ToyBox(string Name)
         {
             Toys.Add(Name);
-
-            Toys.ForEach(Console.WriteLine);
-            /*foreach (string Name in Toys)
-            {
-                Console.WriteLine(Name);
-            } */          
-            //return $"The list of Toys include {Toys}."; 
         }
     }
 }
